2ade00da4a1f4c094b0528ef3287cc575faa5d4a
DanielKnich <127796798+DanielKnich@users.noreply.github.com>
2023-04-04 00:55:47 +0200

    Add files via upload

---
+++ php_03_proste_szablony/app/calc.php
@@ -0,0 +1,53 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+include _ROOT_PATH.'/app/security/sprawdz.php';
+error_reporting(0);
+$kw = $_REQUEST ['kwota'];
+$pr = $_REQUEST ['procent'];
+$lt = $_REQUEST ['lata'];
+
+if ( ! (isset($kw) && isset($pr) && isset($lt))) {
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+if ( $kw == "") {
+	$messages [] = 'Nie podano kwoty pożyczki';
+}
+if ( $pr == "") {
+	$messages [] = 'Nie podano oprocentowania';
+}
+if ( $lt == "") {
+	$messages [] = 'Nie podano okresu pozyczki';
+}
+
+if (empty( $messages )) {
+	if (! is_numeric( $kw )) {
+		$messages [] = 'Kwota pożyczki nie jest liczbą';
+	}
+	
+	if (! is_numeric( $pr )) {
+		$messages [] = 'Oprocentowanie nie jest liczbą';
+	}
+
+	if (! is_numeric( $lt )) {
+		$messages [] = 'Ilość lat nie jest liczbą';
+	}	
+}
+
+if (empty ( $messages )) {
+	
+	$kw = intval($kw);
+	$pr = intval($pr)/100;
+	$lt = intval($lt);
+	for($i=0;$i<$lt;$i++){
+		$kw=$kw*(1+$pr);
+	}
+	$wynik = $kw/(12*$lt);
+}
+
+$page_title = 'zabezpieczony szablon z kalkulatorem';
+$page_description = 'kalkulator kredytowy';
+$page_header = 'Oblicz z Krzysztofem';
+$page_footer = 'Krzysztof Kononowicz był niegdyś kandydatem na prezydenta Białegostoku';
+
+include 'calc_view.php';
